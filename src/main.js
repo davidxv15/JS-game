f1927e54b06d60959146cb5a7983134d7ebb88be
davidxv15 <davidxvaz@gmail.com>
2025-08-03 17:58:09 -0700

    refactor(main.js) imports

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
-import './index.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import './index.css';
+import javascriptLogo from './javascript.svg';
+import viteLogo from '/vite.svg';
+import { setupCounter } from './counter.js';
 
 document.querySelector('#app').innerHTML = `
   <div>
